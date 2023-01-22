bf54dc5f12c06a3ae5c02ac05f26aa2305b1f489
kirill5k <immotional@aol.com>
2023-01-22 20:38:04 +0000

    Add more tests

--- src/test/scala/mongodb/embedded/EmbeddedMongoSpec.scala
+++ src/test/scala/mongodb/embedded/EmbeddedMongoSpec.scala
@@ -2,7 +2,7 @@ package mongodb.embedded
 
 import cats.effect.IO
 import cats.effect.unsafe.IORuntime
-import com.mongodb.MongoCommandException
+import com.mongodb.{MongoCommandException, MongoSecurityException}
 import mongo4cats.bson.{Document, ObjectId}
 import mongo4cats.client.MongoClient
 import mongo4cats.bson.syntax._
@@ -48,6 +48,39 @@ class EmbeddedMongoSpec extends AsyncWordSpec with Matchers with EmbeddedMongo {
           }
           .map(_ => fail("should not reach this"))
           .handleError(_ mustBe a[MongoCommandException])
+      }.unsafeToFuture()(IORuntime.global)
+
+    "return error if user doesn't exist" in
+      withRunningEmbeddedMongo(20717) { connection =>
+        MongoClient
+          .fromConnectionString[IO](s"mongodb://foo:bar@${connection.toString().replaceAll("mongodb:\\/\\/", "")}")
+          .use { client =>
+            for {
+              db <- client.getDatabase("db")
+              coll <- db.getCollection("coll")
+              insertResult <- coll.insertOne(testDoc)
+            } yield insertResult
+          }
+          .map(_ => fail("should not reach this"))
+          .handleError(_ mustBe a[MongoSecurityException])
+      }.unsafeToFuture()(IORuntime.global)
+
+    "start embedded mongodb instance with authed user" in
+      withRunningEmbeddedMongo(20717, "user", "password") { connection =>
+        MongoClient
+          .fromConnection[IO](connection)
+          .use { client =>
+            for {
+              db <- client.getDatabase("db")
+              coll <- db.getCollection("coll")
+              insertResult <- coll.insertOne(testDoc)
+              foundDoc <- coll.find.first
+            } yield (insertResult, foundDoc)
+          }
+          .map { case (insertRes, foundDoc) =>
+            foundDoc mustBe Some(testDoc)
+            insertRes.wasAcknowledged() mustBe true
+          }
       }.unsafeToFuture()(IORuntime.global)
   }
 
