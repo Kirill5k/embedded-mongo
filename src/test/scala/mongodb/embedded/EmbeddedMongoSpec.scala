9b231496322e91990398459d597f3e0411a75a5a
kirill5k <immotional@aol.com>
2023-01-22 13:04:30 +0000

    Add first basic test

---
+++ src/test/scala/mongodb/embedded/EmbeddedMongoSpec.scala
@@ -0,0 +1,45 @@
+package mongodb.embedded
+
+import cats.effect.IO
+import cats.effect.unsafe.IORuntime
+import mongo4cats.bson.{Document, ObjectId}
+import mongo4cats.client.MongoClient
+import mongo4cats.database.MongoDatabase
+import mongo4cats.bson.syntax._
+import org.scalatest.matchers.must.Matchers
+import org.scalatest.wordspec.AsyncWordSpec
+
+import scala.concurrent.Future
+
+class EmbeddedMongoSpec extends AsyncWordSpec with Matchers with EmbeddedMongo {
+
+  val testDoc = Document("_id" := ObjectId(), "stringField" := "string", "intField" := 1)
+
+  "An EmbeddedMongo" should {
+
+    "start embedded mongodb instance in the background" in withEmbeddedMongoDatabase { db =>
+      val result = for {
+        coll <- db.getCollection("coll")
+        insertResult <- coll.insertOne(testDoc)
+        foundDoc <- coll.find.first
+      } yield (insertResult, foundDoc)
+
+      result.map { case (insertRes, foundDoc) =>
+        foundDoc mustBe Some(testDoc)
+        insertRes.wasAcknowledged() mustBe true
+      }
+    }
+  }
+
+  def withEmbeddedMongoDatabase[A](test: MongoDatabase[IO] => IO[A]): Future[A] =
+    withRunningEmbeddedMongo { serverAddress =>
+      MongoClient
+        .fromServerAddress[IO](serverAddress)
+        .use { client =>
+          for {
+            db <- client.getDatabase("db")
+            res <- test(db)
+          } yield res
+        }
+    }.unsafeToFuture()(IORuntime.global)
+}
